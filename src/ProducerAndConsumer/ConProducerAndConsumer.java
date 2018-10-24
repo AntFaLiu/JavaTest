08492b4a22352d7448a132b61a673b7c344f43e4
AntFaLiu <yupeng.liu02@ele.me>
2018-10-24 18:31:14 +0800

    Condition

---
+++ src/ProducerAndConsumer/ConProducerAndConsumer.java
@@ -0,0 +1,158 @@
+package ProducerAndConsumer;
+
+
+//import java.util.concurrent.locks.Condition;
+//import java.util.concurrent.locks.Lock;
+//import java.util.concurrent.locks.ReentrantLock;
+//
+///**
+// * @author zhangliang
+// * <p>
+// * 2016年4月8日 下午5:48:54
+// */
+//public class ConProducerAndConsumer {
+//
+//    final Lock lock = new ReentrantLock();
+//    final Condition condition = lock.newCondition();
+//
+//    public static void main(String[] args) {
+//        // TODO Auto-generated method stub
+//        ConProducerAndConsumer test = new ConProducerAndConsumer();
+//        Producer producer = test.new Producer();
+//        Consumer consumer = test.new Consumer();
+//
+//
+//        consumer.start();
+//        producer.start();
+//    }
+//
+//    class Consumer extends Thread {
+//
+//        @Override
+//        public void run() {
+//            consume();
+//        }
+//
+//        private void consume() {
+//
+//            try {
+//                lock.lock();
+//                System.out.println("我在等一个新信号" + this.currentThread().getName());
+//                condition.await();
+//
+//            } catch (InterruptedException e) {
+//                // TODO Auto-generated catch block
+//                e.printStackTrace();
+//            } finally {
+//                System.out.println("拿到一个信号" + this.currentThread().getName());
+//                lock.unlock();
+//            }
+//
+//        }
+//    }
+//
+//    class Producer extends Thread {
+//
+//        @Override
+//        public void run() {
+//            produce();
+//        }
+//
+//        private void produce() {
+//            try {
+//                lock.lock();
+//                System.out.println("我拿到锁" + this.currentThread().getName());
+//                condition.signalAll();
+//                System.out.println("我发出了一个信号：" + this.currentThread().getName());
+//            } finally {
+//                lock.unlock();
+//            }
+//        }
+//    }
+//}
+
+
+import java.util.PriorityQueue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ConProducerAndConsumer {
+    private int queueSize = 10;
+    private PriorityQueue<Integer> queue = new PriorityQueue<Integer>(queueSize);
+    private Lock lock = new ReentrantLock();
+    private Condition notFull = lock.newCondition();
+    private Condition notEmpty = lock.newCondition();
+
+    public static void main(String[] args) throws InterruptedException {
+        ConProducerAndConsumer test = new ConProducerAndConsumer();
+        Producer producer = test.new Producer();
+        Consumer consumer = test.new Consumer();
+        producer.start();
+        consumer.start();
+        Thread.sleep(0);
+        producer.interrupt();
+        consumer.interrupt();
+    }
+
+    class Consumer extends Thread {
+        @Override
+        public void run() {
+            consume();
+        }
+
+        volatile boolean flag = true;
+
+        private void consume() {
+            while (flag) {
+                lock.lock();
+                try {
+                    while (queue.isEmpty()) {
+                        try {
+                            System.out.println("队列空，等待数据");
+                            notEmpty.await();
+                        } catch (InterruptedException e) {
+                            flag = false;
+                        }
+                    }
+                    queue.poll();                //每次移走队首元素
+                    notFull.signal();
+                    System.out.println("从队列取走一个元素，队列剩余" + queue.size() + "个元素");
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }
+    }
+
+    class Producer extends Thread {
+        @Override
+        public void run() {
+            produce();
+        }
+
+        volatile boolean flag = true;
+
+        private void produce() {
+            while (flag) {
+                lock.lock();
+                try {
+                    while (queue.size() == queueSize) {
+                        try {
+                            System.out.println("队列满，等待有空余空间");
+                            notFull.await();
+                        } catch (InterruptedException e) {
+
+                            flag = false;
+                        }
+                    }
+                    queue.offer(1);        //每次插入一个元素
+                    notEmpty.signal();
+                    System.out.println("向队列取中插入一个元素，队列剩余空间：" + (queueSize - queue.size()));
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }
+    }
+}
--- src/ProducerAndConsumer/ProducerConsumerInJava.java
+++ src/ProducerAndConsumer/ProducerConsumerInJava.java
@@ -100,3 +100,9 @@ class Consumer extends Thread {
         }
     }
 }
+
+//1. 你可以使用wait和notify函数来实现线程间通信。你可以用它们来实现多线程（>3）之间的通信。
+//2. 永远在synchronized的函数或对象里使用wait、notify和notifyAll，不然Java虚拟机会生成 IllegalMonitorStateException。
+//3. 永远在while循环里而不是if语句下使用wait。这样，循环会在线程睡眠前后都检查wait的条件，并在条件实际上并未改变的情况下处理唤醒通知。
+//4. 永远在多线程间共享的对象（在生产者消费者模型里即缓冲区队列）上使用wait。
+//5. 基于前文提及的理由，更倾向用 notifyAll()，而不是 notify()。
